4a0413e472274869ae1f982ce9c4bcdcbc346ffc
sishubjoshi <shisub1999@gmail.com>
2019-12-20 01:32:00 +0530

    db key problem | require better solution

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 module.exports = {
-	mongoURI: 'mongodb://localhost/phonebook'
-	// mongoURI:
-	// 	'mongodb+srv://joshi:1234@cluster0-y7xmo.mongodb.net/test?ssl=true&authSource=admin&replicaSet=Cluster0-shard-0',
-	// secretOrKey: 'secret'
+	// mongoURI: 'mongodb://localhost/phonebook'
+	mongoURI:
+		'mongodb+srv://joshi:1234@cluster0-y7xmo.mongodb.net/test?ssl=true&authSource=admin&replicaSet=Cluster0-shard-0',
+	secretOrKey: 'secret'
 };
